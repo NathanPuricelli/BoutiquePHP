c56a6552bb597ea4a0c4263751617b11fb915075
PURICELLI NATHAN P1907453 <nathan.puricelli@etu.univ-lyon1.fr>
2022-01-07 18:56:55 +0100

    Début du panier , fonctions de bd ok sauf pour fusionner les meme produits

--- Model/Cart.php
+++ Model/Cart.php
@@ -1,19 +1,68 @@
 <?php
 require_once 'Model.php';
 class Cart extends Model {
-    public function getCartContent()
-    {
-        /*
-        Est-ce que la commande en cours est stockée dans un post ?
 
-        $sql = "SELECT * FROM products WHERE id = ?";
-        $product = $this->executerRequete($sql, array($id));
+    public function getOrderId($id_customer)
+    {
+        $sql = "select id from orders where customer_id = ? and status != 10 ";
+        $id = $product = $this->executerRequete($sql, array($id_customer));
+        if ($id->rowCount() > 0) {
+            return $id;
+        }
+        else {
+            return false; // si on essaie de regarder une commande qui n'existe pas on le fait savoir
+        }
+
+    }
+    
+    public function getCartContent($id_order)
+    {
+        $sql = "select * from products P where P.id IN
+        (SELECT product_id from orderitems where order_id = ?)";
+        $product = $this->executerRequete($sql, array($id_order));
         if ($product->rowCount() > 0)
         {
-            return $product->fetch();
+            return $product;
         }
         else
-            throw new Exception("Aucun billet ne correspond à l'identifiant '$id'");
-        */
+            return;
+    }
+
+
+    public function createOrder($id_customer, $session, $registered) // l'id custommer sera 0 si l'utilisateur n'est pas connecté.
+    {
+        $sql = "insert into orders(customer_id, date, registered, session, status) 
+        VALUES (?, ?, ?, ?, 0)";
+        try{
+            $this->executerRequete($sql, array($id_customer, date("Y")."-".date("m")."-".date("d"), $session, $registered));
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function addProductToOrder($id_Order, $id_product, $quantity){
+        $sql = "insert into orderitems (order_id, product_id, quantity)
+        values (?, ?, ?)";
+        try{
+            $this->executerRequete($sql, array($id_Order, $id_product, $quantity));
+            $this->mergeSameItemsCart($id_Order, $id_product);
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    private function mergeSameItemsCart($id_Order, $id_product)
+    {
+        $sql = "";
+        try{
+            $this->executerRequete($sql, array($id_Order, $id_product));
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+
     }
 };
--- README.md
+++ README.md
@@ -43,3 +43,6 @@ Controller :
         -Gérer bug sur la page inscription quand on efface ses données de navigation depuis celle ci
 
 
+On ajoute un item dans le panier : 
+    On créé une commande avec cet item et la quantité.
+    Si l'utilisateur n'est pas connecté, on créé une commande avec la variable de session id, puis on récupere l'id de la commande qu'on met dans $SESSION["SESS_ORDERNUM"]. On track apres avec cette session. Quand on se connecte on merge les 2 commandes en regardant session.
