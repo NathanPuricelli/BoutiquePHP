4c2e85757cba7ee5af9a049069e67c18cd7c8983
PURICELLI NATHAN P1907453 <nathan.puricelli@etu.univ-lyon1.fr>
2022-01-11 01:37:16 +0100

    controller checkout et début de la vue

---
+++ View/ViewCheckout.php
@@ -0,0 +1,13 @@
+<?php $this->title = "Panier"; ?>
+<!-- Formulaire de remplissage -->
+
+<?= "Prix à payer : '$price'" ?>
+
+<h1>Veuillez saisir les informations</h1>
+<div class = formCheckout>
+    <form>
+        <input type = "text"
+    </form>
+
+
+</div>
