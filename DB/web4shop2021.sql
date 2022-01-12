2147d6d1269a1cd1fb942137593eaddfc8c0d2aa
PURICELLI NATHAN P1907453 <nathan.puricelli@etu.univ-lyon1.fr>
2022-01-12 03:07:34 +0100

    correction d'une erreur de syntaxe

--- DB/web4shop2021.sql
+++ DB/web4shop2021.sql
@@ -42,7 +42,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 (1, 'john', 'f18bd055eec95965ee175fa9badd35ae6dbeb98f'), 
-(2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'), ;
+(2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
 
 -- --------------------------------------------------------
 
